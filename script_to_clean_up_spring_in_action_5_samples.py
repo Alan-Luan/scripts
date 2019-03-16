7399199c31c203556ba2a7a7d44e301cbc0f1c0a
superAlan <depeng.luan.cmu@gmail.com>
2019-03-15 23:51:49 -0700

    add feature to remove number tag comment

--- script_to_clean_up_spring_in_action_5_samples.py
+++ script_to_clean_up_spring_in_action_5_samples.py
@@ -1,6 +1,3 @@
-# python script to clean up source code of spring-in-action-5-samples:
-# https://github.com/habuma/spring-in-action-5-samples
-
 import os
 
 def pywalker(path):
@@ -13,15 +10,25 @@ def pywalker(path):
 
 
 def process_file(path):
-	f = open(path, "r")
-	lines = f.readlines()
-	f.close()
-	f = open(path, "w")
-	for line in lines:
-		if ("tag::" not in line) and ("end::" not in line):
-			f.write(line)
-	f.close()
+    f = open(path, "r")
+    lines = f.readlines()
+    f.close()
+    f = open(path, "w")
+    for line in lines:
+        if ("// <" in line):
+            substring_index = line.index("// <")
+            line = line[0: substring_index] + "\n"
+        f.write(line)
+    f.close()
+
+#	for line in lines:
+#		if ("tag::" not in line) and ("end::" not in line):
+#			f.write(line)
+#	f.close()
 
 
+# python script to clean up source code of spring-in-action-5-samples:
+# https://github.com/habuma/spring-in-action-5-samples
 if __name__ == '__main__':
+    # replace path here
 	pywalker('path/to/spring-in-action-5-samples/folder')
